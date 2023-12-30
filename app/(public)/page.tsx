a7d55762baf5b741c64ed0b0bbd0ad004b3f9094
Om Tekade <omtekade914@gmail.com>
2023-12-30 12:25:05 +0530

    bottom margin add to contact form on mobile devices

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <div className="sm:flex-1 sm:h-[90vh] h-full flex flex-col sm:px-20 mt-4 sm:mt-0 text-center sm:text-start p-2 sm:p-0">
               <Hero />
             </div>
-            <div className="sm:flex-1 sm:h-[90vh] h-full sm:px-8 px-2 flex items-center justify-center p-2 sm:p-0">
+            <div className="sm:flex-1 sm:h-[90vh] h-full sm:px-8 px-2 flex items-center justify-center p-2 sm:p-0 mb-8 sm:mb-0">
               <Contact />
             </div>
         </div>
